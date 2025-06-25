43849c287f428334d16fc3cf7b1f40116062841c
kdh6979dd <kdh6979dd@gmail.com>
2025-06-25 19:53:38 +0900

    Add files via upload

---
+++ 소스20250625.cpp
@@ -0,0 +1,129 @@
+//#include <stdio.h>
+//
+//int main()
+//{
+//	printf("안녕하세요\n");
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	printf("안녕하세요");
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	printf("안녕하세요\n안녕하세요\n안녕하세요\n");
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	printf("안녕하세요\n");
+//	printf("안녕하세요");
+//	printf("안녕하세요\n");
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	printf("사과 1개\n");
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int apple = 1;
+//	printf("사과 %d개\n", apple);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	printf("키보드 3개\n");
+//	printf("마우스 2개\n");
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int keyboard = 3;
+//	int mouse = 2;
+//	printf("키보드: %d개\n", keyboard);
+//	printf("마우스: %d개\n", mouse);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int coin1=500;
+//	int coin2=1000;
+//	printf("%d + %d = %d\n",
+//		coin1, coin2, coin1+coin2);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	char A;
+//	A = 'A';
+//	printf("A=%c,A=%d\n",A,A);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	double pi;
+//	pi=3.14;
+//	float e = 2.718;
+//	printf("pi=%.2lf,e=%.3f.. \n",pi,e);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int r = 5;
+//	double pi=3.14;
+//	//원의넓이: pi*r*r
+//	float area = pi * r * r;
+//	//원둘레: 2*pi*r
+//	float circumference = 2*pi*r;
+//	printf("area = %.2f, circumference = %.2f\n", area, circumference);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	//trapezium's area = ( base_line + upper_edge ) * height / 2
+//	int baseline=3;
+//	int upperedge=5;
+//	double height=7;
+//	double trapeziumarea = (baseline + upperedge) * height / 2;
+//	printf("trapezium's area = %.2f\n", trapeziumarea);
+//	return 0;
+//}
