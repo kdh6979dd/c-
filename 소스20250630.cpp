2729eb200c85c3a0e0892e54e99b0f5fb6bbbc4f
kdh6979dd <kdh6979dd@gmail.com>
2025-06-30 19:53:07 +0900

    Add files via upload

---
+++ 소스20250630.cpp
@@ -0,0 +1,170 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//int main()
+//{
+//	int k;
+//	printf("원하는 숫자를 입력 :");
+//	scanf("%d", &k); //vs: scanf_s
+//	printf("입력한 숫자는 : %d\n", k);
+//	return 0;
+//}
+
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//int main()
+//{
+//	int y, x;
+//	printf("더하고 싶은 값 2개 입력 :");
+//	scanf("%d%d", &y, &x); //scanf_s
+//	printf("두 값의 합은 %d입니다.\n", y+x);
+//	return 0;
+//}
+
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//int main()
+//{
+//	char a,b,c;
+//	printf("문자 두 개 입력 :");
+//	scanf("%c%c%c", &a, &b, &c);
+//	printf("문자출력 : %c%c%c\n", a,b,c);
+//	return 0;
+//}
+
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//int main()
+//{
+//	int r;
+//	float pi = 3.14;
+//	//원의 길이(원주) : 2 * pi * r 
+//	//원의 넓이 : pi * r * r
+//	printf("원의 반지름 입력 :");
+//	scanf("%d", &r);
+//	printf("원의 넓이는 %.2lf입니다.\n", pi * r * r);
+//	printf("원의 길이는 %.2lf입니다.\n", 2 * pi * r);
+//	return 0;
+//}
+
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//int main()
+//{
+//	float W1;
+//	float W2;
+//	float H;
+//	//사다리꼴의 넓이 : (W2+W1)*H/2
+//	printf("윗변 입력 :");
+//	scanf("%f", &W2);
+//	printf("밑변 입력 :");
+//	scanf("%f", &W1);
+//	printf("높이 입력 :");
+//	scanf("%f", &H);
+//	printf("사다리꼴의 넓이는 %.2f입니다.\n", (W2 + W1) * H / 2);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int a = 15;
+//	int b = 3;
+//	printf("%d+%d=%d\n", a, b, a + b);
+//	printf("%d-%d=%d\n", a, b, a - b);
+//	printf("%d*%d=%d\n", a, b, a * b);
+//	printf("%d/%d=%d\n", a, b, a / b);
+//	printf("%d%%%d=%d\n", a, b, a % b);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int number = 1;
+//	number = number + 3; printf("%2d\n", number);
+//	number -= 2; printf("%2d\n", number);
+//	number --; printf("%2d\n", number);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int a = 0;
+//	printf("%d\n", a++);
+//	printf("%d\n", a);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int a = 0;
+//	printf("%d\n", ++a);
+//	printf("%d\n", a);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int variable_1;
+//	int variable_2;
+//	printf("Value of the first variable :");
+//	scanf("%d", &variable_1);
+//	printf("Value of the second variable :");
+//	scanf("%d", &variable_2);
+//	printf("%d+%d=%d\n", variable_1, variable_2, variable_1 + variable_2);
+//	printf("%d-%d=%d\n", variable_1, variable_2, variable_1 - variable_2);
+//	printf("%d*%d=%d\n", variable_1, variable_2, variable_1 * variable_2);
+//	printf("%d/%d=%d\n", variable_1, variable_2, variable_1 / variable_2);
+//	printf("%d%%%d=%d\n", variable_1, variable_2, variable_1 % variable_2);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int variable_1;
+//	int variable_2;
+//
+//	printf("Value of the first variable :");
+//	scanf("%d", &variable_1);
+//	printf("Value of the second variable :");
+//	scanf("%d", &variable_2);
+//
+//	printf("%d\n", variable_1 + variable_2);
+//	printf("%d\n", variable_1 - variable_2);
+//	printf("%d\n", variable_1 * variable_2);
+//	printf("%d\n", variable_1 / variable_2);
+//	printf("%d\n", variable_1 % variable_2);
+//
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int first = 1 % 4;
+//	printf("%d=1%%4\n", first);
+//	int second = 5 % 3;
+//	printf("%d=5%%3\n", second);
+//	int third = 4 % 2;
+//	printf("%d=4%%2\n", third);
+//	return 0;
+//}
+
+#include <stdio.h>
+
+int main()
+{
+	int number1, number2;
+	scanf("%d%d", &number1, number2);
+	printf("")
+}
