a2d129550e1df19c5acf7453670a2d959ff96bd2
kdh6979dd <kdh6979dd@gmail.com>
2025-07-02 19:57:05 +0900

    Add files via upload

---
+++ 소스20250702.cpp
@@ -0,0 +1,62 @@
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int number1, number2;
+//	scanf("%d%d", &number1, &number2);
+//	printf("%d =number1%%4\n", number1%4);
+//	printf("%d =number2%%3\n", number2%3);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int first = 1 % 3;
+//	printf("%d=1%%3\n", first);
+//	int second = 2 % 3;
+//	printf("%d=2%%3\n", second);
+//	int third = 3 % 3;
+//	printf("%d=3%%3\n", third);
+//	int fourth = 4 % 3;
+//	printf("%d=4%%3\n", fourth);
+//	int fifth = 5 % 3;
+//	printf("%d=5%%3\n", fifth);
+//	int sixth = 6 % 3;
+//	printf("%d=6%%3\n", sixth);
+//
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int number;
+//	scanf("%d", &number);
+//	printf("%d\n", number%50);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int data1 = 6 / 2;
+//	printf("%d = 6 / 2\n", data1);
+//	int data2 = 6 / 3;
+//	printf("%d = 6 / 3\n", data2);
+//	int data3 = 6 / 4;
+//	printf("%d = 6 / 4\n", data3);
+//	return 0;
+//}
+
+#include <stdio.h>
+
+int main()
+{
+	int number1, number2;
+	scanf("%d%d")
+	return 0;
+}
