6428b650dfc0ffe848f717add84a1f0fa76ff3f5
kdh6979dd <kdh6979dd@gmail.com>
2025-08-20 17:19:46 +0900

    Add files via upload

---
+++ 8월9회차숙제.cpp
@@ -0,0 +1,34 @@
+//#include <stdio.h>
+//void main()
+//{
+//    int i, k;
+//    for (i = 0; i < 3; i++) {
+//        for (k = 0; k < 2; k++) {
+//            printf("중첩 for문입니다. (i값: %d, k값: %d)\n", i, k);
+//        }
+//    }
+//}
+
+//#include <stdio.h>
+//void main()
+//{
+//    int i, k;
+//    for (i = 2; i <= 9; i++) {
+//        for (k = 1; k <= 9; k++) {
+//            printf("%d X %d = %d \n", i, k, i * k);
+//        }
+//        printf("\n");
+//    }
+//}
+
+//#include <stdio.h>
+//void main()
+//{
+//    int i, k;
+//    for (i = 1; i <= 9; i++) {
+//        for (k = 2; k <= 9; k++) {
+//            printf("%2dX%2d=%2d ", k, i, k * i);
+//        }
+//        printf("\n");
+//    }
+//}
