209493e7939951c2d1cacf207fda6f5e25693851
kdh6979dd <kdh6979dd@gmail.com>
2025-09-01 17:22:48 +0900

    Add files via upload

---
+++ 20250827숙제.cpp
@@ -0,0 +1,95 @@
+/* 문제 2번 */
+//#include <stdio.h>
+//
+//int main() {
+//    printf("   *\n");
+//    printf("  ***\n");
+//    printf(" *****\n");
+//    printf("*******\n");
+//    return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main() {
+//    for (int i = 0; i < 3; i++) printf(" ");
+//    printf("*\n");
+//
+//    for (int i = 0; i < 2; i++) printf(" ");
+//    printf("***\n");
+//
+//    for (int i = 0; i < 1; i++) printf(" ");
+//    printf("*****\n");
+//
+//    printf("*******\n");
+//    return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main() {
+//    int outer = 4;
+//    for (int y = 1; y <= outer; y++) {
+//        for (int s = 1; s <= outer - y; s++) printf(" ");
+//        for (int star = 1; star <= 2 * y - 1; star++) printf("*");
+//        printf("\n");
+//    }
+//    return 0;
+//}
+
+/* 문제 3번 */
+
+//#include <stdio.h>
+//
+//int main() {
+//    printf("  *\n");
+//    printf(" ***\n");
+//    printf("*****\n");
+//    printf(" ***\n");
+//    printf("  *\n");
+//    return 0;
+//}
+
+/* 문제 4번 */
+
+//#include <stdio.h>
+//
+//int main() {
+//    printf("   1\n");
+//    printf("  22\n");
+//    printf(" 333\n");
+//    printf("4444\n");
+//    return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main() {
+//    for (int s = 0; s < 3; s++) printf(" ");
+//    printf("1\n");
+//
+//    for (int s = 0; s < 2; s++) printf(" ");
+//    printf("22\n");
+//
+//    for (int s = 0; s < 1; s++) printf(" ");
+//    printf("333\n");
+//
+//    printf("4444\n");
+//    return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main() {
+//    int outer = 4;
+//    for (int i = 1; i <= outer; i++) {
+//        for (int s = 1; s <= outer - i; s++) {
+//            printf(" ");
+//        }
+//        for (int j = 1; j <= i; j++) {
+//            printf("%d", i);
+//        }
+//        printf("\n");
+//    }
+//    return 0;
+//}
